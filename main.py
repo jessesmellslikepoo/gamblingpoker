00767b0f8642f3238011bfde388b952f5b5a88f0
shardruce <jessemastrogiacomo@gmail.com>
2025-01-12 17:17:42 -0500

    Update main.py
    class is finished. Most likely needs several improvements to the code including possible logic redundancies and errors. It also needs comments to document the code.

--- main.py
+++ main.py
@@ -57,46 +57,82 @@ class Card():
         check_player_cards = [card for card in cls.player_cards if isinstance(card, Card)]
         # sorts cards based on a sorting key, that each Card object should be sorted based on the numeric value, and then on what each suit alphabetically is.
         sorted_cards = sorted(check_player_cards, key = lambda card : (card.get_val(), card.get_suit()))
-        prev_loc = None
         card_run = False
-        is_pair = False
         three_kind = False
+        four_kind = False
+        run_fail = False
+        is_flush = True
+        roy_flush = False
         pair_count = 0
+        num_count = 1
         for card in sorted_cards:
             if isinstance(card, Card):
                 num_of_cards.append(card.get_val())
                 suit_of_cards.append(card.get_suit())
 
-        # checking for a run 
+        # checking for a run and a pair  
         for i in range(len(num_of_cards) - 1):
-            if prev_loc is not None:
-                if prev_loc == num_of_cards[i] - 1:
-                    prev_loc = num_of_cards[i]
-                else:
-                    prev_loc = None
-                    break
+            # run checking...
+            if not run_fail and num_of_cards[i] != num_of_cards[i + 1] - 1:
+                run_fail = True
+            # pair checking... 
+            if num_of_cards[i] == num_of_cards[i + 1]:
+                num_count += 1
             else:
-                prev_loc = num_of_cards[i]
-        if prev_loc == num_of_cards[-1]:
+                pair_count += num_count // 2
+                num_count = 1
+        pair_count += num_count // 2
+        if not run_fail and num_of_cards[-2] + 1 == num_of_cards[-1]:
             card_run = True
-        if card_run:
+        for i in range(len(sorted_cards) - 1):
+            if num_of_cards[0] == 10 and card_run: 
+                roy_flush = True
+                break
+            if sorted_cards[i].get_suit() != sorted_cards[i + 1].get_suit():
+                is_flush = False
+                break
+
+        if roy_flush:
+            points = (cls.get_total_card_val() + 300) * cls.markiplier
+        elif card_run and is_flush:
+            points = (cls.get_total_card_val() + 250) * cls.markiplier
+            return points
+        elif card_run:
             points = (cls.get_total_card_val() + 200) * cls.markiplier
             return points
 
-        # one pair of cards/two pairs.
-        for i in range(len(num_of_cards) - 1):
-            if num_of_cards[i] == num_of_cards[i + 1]:
-                pair_count += 1
-        if pair_count > 0:
-            is_pair = True       
-        if is_pair:
-            points = cls.get_total_card_val() + 50 * pair_count * cls.markiplier
+        if is_flush:
+            points = (cls.get_total_card_val() + 50) * cls.markiplier
             return points
+        
 
-        # three of a kind
-        for i in range(len(num_of_cards) - 2):
-            if num_of_cards[i] == num_of_cards[i + 1] == num_of_cards[i + 2]:
+        # three of a kind/four of a kind
+        for i in range(len(num_of_cards) - 3):
+            if num_of_cards[i] == num_of_cards[i + 1] == num_of_cards[i + 2] == num_of_cards[i + 3]:
+                four_kind = True 
+                break
+            elif num_of_cards[i] == num_of_cards[i + 1] == num_of_cards[i + 2]:
                 three_kind = True
                 break
-        if (three_kind):
+
+        # checks for pairs, full house, and three of a kind/four of a kind.
+        if pair_count > 0:
+            if three_kind:
+                # full house
+                points = cls.get_total_card_val() + 225 * pair_count * cls.markiplier
+                return points
+            # regular pair 
+            points = cls.get_total_card_val() + 50 * pair_count * cls.markiplier
+            return points 
+        if three_kind:
             points = (cls.get_total_card_val() + 60) * cls.markiplier
+            return points
+        if four_kind:
+            points = (cls.get_total_card_val() + 80) * cls.markiplier
+            return points
+
+        for i in range(len(num_of_cards)):
+            if num_of_cards[i] == 13:
+                high_card = True
+        if high_card:
+            points (cls.get_total_card_val + 20) * cls.markiplier
